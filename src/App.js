47e80fe6284501c48d961fc977aa50ff1595d386
ngainska <87204580+ngainska@users.noreply.github.com>
2021-11-12 20:29:56 +0000

    added github link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+     <footer>
+       This project was coded by <a href="https://admiring-sinoussi-3cc72d.netlify.app/">Nicole Gainska</a> and is {""}
+     <a href="https://github.com/ngainska/new-react-weather" target="_blank" rel="noreferrer">
+open-sourced on GitHub
+     </a>
+     </footer>
     </div>
   );
 }
