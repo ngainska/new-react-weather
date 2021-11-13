cb470004aad72fca0c7f5273383407890932b1fd
ngainska <87204580+ngainska@users.noreply.github.com>
2021-11-13 17:30:21 +0000

    changed clearfix to d-flex
    not sure why clearfix didn't work...

--- src/Weather.js
+++ src/Weather.js
@@ -21,7 +21,7 @@ export default function Weather(){
 
             <div className="row mt-3">
                 <div className="col-6">
-                    <div className="clearfix">
+                    <div className="d-flex">
                     <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
                     alt="Mostly Cloudy"
                     className="float-left"/>
