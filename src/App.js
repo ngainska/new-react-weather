9e90208b0fc5c8661defdc2dfb322d76edcedcb3
ngainska <87204580+ngainska@users.noreply.github.com>
2021-11-13 17:23:02 +0000

    added CSS
    why doesn't the clearfix and float-left work when the code is exactly the same?...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1>Weather App</h1>
+     
      <Weather/>
      <footer>
        This project was coded by <a href="https://admiring-sinoussi-3cc72d.netlify.app/">Nicole Gainska</a> and is {""}
--- src/Weather.js
+++ src/Weather.js
@@ -7,9 +7,9 @@ export default function Weather(){
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder="Insert a city.." className="form-control" /> </div>
+                <input type="search" placeholder="Insert a city.." className="form-control" autoFocus="on"/> </div>
                 <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary"/>
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
                 </div>
                 </div>
             </form>
@@ -18,12 +18,18 @@ export default function Weather(){
                 <li>Saturday 16:00</li>
                 <li>Mostly Cloudy</li>
             </ul>
-            <div className="row">
+
+            <div className="row mt-3">
                 <div className="col-6">
+                    <div className="clearfix">
                     <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                    alt="Mostly Cloudy">
-                    </img>
-                    6°C
+                    alt="Mostly Cloudy"
+                    className="float-left"/>
+                    <div className="float-left">
+                    <span className="temperature">6</span>
+                    <span className="unit">°C</span>
+                    </div>
+                    </div>
                 </div>
                 <div className="col-6">
                     <ul>
