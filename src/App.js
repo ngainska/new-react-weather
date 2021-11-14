102c9e2b5bcb2f0486a14538c8890b8172da93c0
ngainska <87204580+ngainska@users.noreply.github.com>
2021-11-14 12:49:43 +0000

    added unit conversion

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
        This project was coded by <a href="https://admiring-sinoussi-3cc72d.netlify.app/">Nicole Gainska</a> and is {""}
      <a href="https://github.com/ngainska/new-react-weather" target="_blank" rel="noreferrer">
 open-sourced on GitHub
-     </a>
+     </a> and <a href="https://dreamy-kare-fc405e.netlify.app/">hosted on Netlify</a>
      </footer>
      </div>
     </div>
