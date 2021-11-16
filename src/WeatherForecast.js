00aea00110851bf5e453d7522052bade2a75882b
ngainska <87204580+ngainska@users.noreply.github.com>
2021-11-16 21:16:10 +0000

    refactored code, fixed warnings
    everything working EXCEPT forecast doesn't convert units...

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,10 +18,19 @@ function handleResponse(response){
     setReady(true);
 }
 
+function load(){
+     const apiKey = "b6a67f67579bcd300971f2f49b91d214";
+    let lat= props.coordinates.lat;
+    let lon=props.coordinates.lon;
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+}
+
 if (ready) { 
     return (
     <div className="WeatherForecast">
-        <div className="row">
+        <div className="row mt-3">
             {forecast.map(function(dailyForecast,index){
                 if (index <5) {
                 return(
@@ -29,19 +38,14 @@ if (ready) {
                 <WeatherForecastDay data={dailyForecast}/>
             </div>
             );
-            }
+            } else {return null;}
             })}
             
         </div>
     </div>);
 }
  else {
-    const apiKey = "b6a67f67579bcd300971f2f49b91d214";
-    let lat= props.coordinates.lat;
-    let lon=props.coordinates.lon;
-    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
+   load();
     
     return null;
    }
